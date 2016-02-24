aabfe3695e9b69f98c1f1fedc07705f22e71c41b
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2016-02-24 03:16:12 +0200

    Fix things not being wrapped on initial world spawn point creation

--- src/main/java/fi/dy/masa/paintedbiomes/event/PaintedBiomesEventHandler.java
+++ src/main/java/fi/dy/masa/paintedbiomes/event/PaintedBiomesEventHandler.java
@@ -11,12 +11,6 @@ import net.minecraft.world.gen.ChunkProviderGenerate;
 import net.minecraft.world.gen.ChunkProviderHell;
 import net.minecraft.world.gen.ChunkProviderServer;
 
-import fi.dy.masa.paintedbiomes.PaintedBiomes;
-import fi.dy.masa.paintedbiomes.config.Configs;
-import fi.dy.masa.paintedbiomes.image.ImageHandler;
-import fi.dy.masa.paintedbiomes.world.GenLayerBiomeGeneration;
-import fi.dy.masa.paintedbiomes.world.GenLayerBiomeIndex;
-import fi.dy.masa.paintedbiomes.world.WorldChunkManagerPaintedBiomes;
 import net.minecraftforge.event.terraingen.WorldTypeEvent;
 import net.minecraftforge.event.world.WorldEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -24,6 +18,13 @@ import net.minecraftforge.fml.common.gameevent.TickEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent.ServerTickEvent;
 import net.minecraftforge.fml.relauncher.ReflectionHelper;
 import net.minecraftforge.fml.relauncher.ReflectionHelper.UnableToAccessFieldException;
+
+import fi.dy.masa.paintedbiomes.PaintedBiomes;
+import fi.dy.masa.paintedbiomes.config.Configs;
+import fi.dy.masa.paintedbiomes.image.ImageHandler;
+import fi.dy.masa.paintedbiomes.world.GenLayerBiomeGeneration;
+import fi.dy.masa.paintedbiomes.world.GenLayerBiomeIndex;
+import fi.dy.masa.paintedbiomes.world.WorldChunkManagerPaintedBiomes;
 
 
 public class PaintedBiomesEventHandler
@@ -40,6 +41,16 @@ public class PaintedBiomesEventHandler
     @SubscribeEvent
     public void onWorldLoad(WorldEvent.Load event)
     {
+        overrideChunkProvider(event.world);
+        overrideWorldChunkManager(event.world);
+    }
+
+    @SubscribeEvent
+    public void onCreateWorldSpawn(WorldEvent.CreateSpawnPosition event)
+    {
+        // The initial world spawn position is created before the WorldEvent.Load fires, so we
+        // need this event to cover that case. Otherwise a newly created world (no existing level.dat file yet)
+        // will have a mess of regular terrain generation chunks near the spawn chunk...
         overrideChunkProvider(event.world);
         overrideWorldChunkManager(event.world);
     }
@@ -82,16 +93,13 @@ public class PaintedBiomesEventHandler
         {
             if (dimension == i)
             {
-                // Re-initialize the ImageHandler after a world loads, to update config values etc.
-                ImageHandler imageHandler = ImageHandler.getImageHandler(dimension).init();
-
-                overrideWorldChunkManager(dimension, world, imageHandler);
+                overrideWorldChunkManager(dimension, world);
                 break;
             }
         }
     }
 
-    private static void overrideWorldChunkManager(int dimension, World world, ImageHandler imageHandler)
+    private static void overrideWorldChunkManager(int dimension, World world)
     {
         if (world.getWorldChunkManager() instanceof WorldChunkManagerPaintedBiomes)
         {
@@ -103,6 +111,9 @@ public class PaintedBiomesEventHandler
 
         try
         {
+            // Re-initialize the ImageHandler when a world loads, to update config values etc.
+            ImageHandler imageHandler = ImageHandler.getImageHandler(dimension).init();
+
             WorldChunkManager newWCM = new WorldChunkManagerPaintedBiomes(world, world.getWorldChunkManager(), imageHandler);
             ReflectionHelper.setPrivateValue(WorldProvider.class, world.provider, newWCM, "field_76578_c", "c", "worldChunkMgr");
         }
