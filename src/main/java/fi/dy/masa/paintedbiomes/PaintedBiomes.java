8027e914e3cd8be09fa9a9a00e7ad15e951ae18f
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2017-08-03 02:52:31 +0300

    Sign the JAR.

--- src/main/java/fi/dy/masa/paintedbiomes/PaintedBiomes.java
+++ src/main/java/fi/dy/masa/paintedbiomes/PaintedBiomes.java
@@ -3,6 +3,7 @@ package fi.dy.masa.paintedbiomes;
 import org.apache.logging.log4j.Logger;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLFingerprintViolationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerAboutToStartEvent;
@@ -11,8 +12,8 @@ import fi.dy.masa.paintedbiomes.event.PaintedBiomesEventHandler;
 import fi.dy.masa.paintedbiomes.reference.Reference;
 
 
-@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION,
-    acceptableRemoteVersions="*", acceptedMinecraftVersions = "[1.11,1.11.2]")
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION, certificateFingerprint = Reference.FINGERPRINT,
+    acceptableRemoteVersions="*", acceptedMinecraftVersions = "[1.11.2]")
 public class PaintedBiomes
 {
     @Mod.Instance(Reference.MOD_ID)
@@ -46,5 +47,24 @@ public class PaintedBiomes
         // This means that in single player you can just save and exit to main menu,
         // make changes to the config and then load a world and have the new configs be used.
         Configs.reload();
+    }
+
+    @Mod.EventHandler
+    public void onFingerPrintViolation(FMLFingerprintViolationEvent event)
+    {
+        // Not running in a dev environment
+        if (event.isDirectory() == false)
+        {
+            logger.warn("*********************************************************************************************");
+            logger.warn("*****                                    WARNING                                        *****");
+            logger.warn("*****                                                                                   *****");
+            logger.warn("*****   The signature of the mod file '{}' does not match the expected fingerprint!     *****", event.getSource().getName());
+            logger.warn("*****   This might mean that the mod file has been tampered with!                       *****");
+            logger.warn("*****   If you did not download the mod {} directly from Curse/CurseForge,       *****", Reference.MOD_NAME);
+            logger.warn("*****   or using one of the well known launchers, and you did not                       *****");
+            logger.warn("*****   modify the mod file at all yourself, then it's possible,                        *****");
+            logger.warn("*****   that it may contain malware or other unwanted things!                           *****");
+            logger.warn("*********************************************************************************************");
+        }
     }
 }
