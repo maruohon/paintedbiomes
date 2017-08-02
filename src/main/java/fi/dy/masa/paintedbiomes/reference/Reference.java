33b089ab1d49a4115059c648442580da0d723a99
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2017-08-03 02:52:31 +0300

    Sign the JAR. Update Forge. Bump version to 1.12.0-0.5.1

--- src/main/java/fi/dy/masa/paintedbiomes/reference/Reference.java
+++ src/main/java/fi/dy/masa/paintedbiomes/reference/Reference.java
@@ -5,6 +5,7 @@ public class Reference
     public static final String MOD_ID = "paintedbiomes";
     public static final String MOD_NAME = "Painted Biomes";
     public static final String MOD_VERSION = "@MOD_VERSION@";
+    public static final String FINGERPRINT = "2b03e1423915a189b8094816baa18f239d576dff";
     public static final String PROXY_CLASS_CLIENT = "fi.dy.masa.paintedbiomes.proxy.ClientProxy";
     public static final String PROXY_CLASS_SERVER = "fi.dy.masa.paintedbiomes.proxy.CommonProxy";
 }
